0bbf196f0ea5479bb7a16f942188f0964959a4c6
Kevin <kevinlenske@proton.me>
2023-01-02 16:28:51 -0600

    added carousel

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/Layout/Header'
+import Hero from '../components/Layout/Hero'
 function App() {
 
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,36 @@ module.exports = {
     './node_modules/preline/dist/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xxs: '0.625rem',
+      },
+      height: {
+        98: '26.0625rem',
+        100: '28rem',
+        102: '30rem',
+        104: '32rem',
+        108: '36rem',
+        112: '40rem',
+        116: '44rem',
+      },
+      screens: {
+        '-2xl': { max: '1535px' },
+        // => @media (max-width: 1535px) { ... }
+
+        '-xl': { max: '1279px' },
+        // => @media (max-width: 1279px) { ... }
+
+        '-lg': { max: '1023px' },
+        // => @media (max-width: 1023px) { ... }
+
+        '-md': { max: '767px' },
+        // => @media (max-width: 767px) { ... }
+
+        '-sm': { max: '639px' },
+        // => @media (max-width: 639px) { ... }
+      },
+    },
   },
   plugins: [require('preline/plugin')],
 };
