6bd85763013262b63be733ac7fc2c5b36ee5db83
Kevin <kevinlenske@proton.me>
2023-03-19 03:05:24 -0500

    changes

---
+++ functions/fetch-movies/fetch-movies.js
@@ -0,0 +1,38 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+
+// --> ?name="bob" = event.queryStringParameters.name
+// --> ?name="bob"&last_name = event.queryStringParameters.last_name
+
+// --> ?access_key="123"&query=1234,1234&forecast_days=4
+
+const axios = require('axios')
+
+const handler = async (event) => {
+  const { startingParams, categoryParams, id, page, searchQuery, gte, lte, gteAD, lteAD } = event.queryStringParameters;
+  
+  // const API_SECRET = process.env.API_SECRET
+  // const API_SECRET = '8e6ba047d3bc0b9dddf8392f32410006'
+  const API_SECRET = process.env.API_SECRET
+  const url = `https://api.themoviedb.org/3/${startingParams}${id ? `/${id}` : ''}${categoryParams ? `/${categoryParams}` : ''}?api_key=${API_SECRET}&language=en-US&${gte ? `primary_release_date.gte=${gte}&` : ''}${lte ? `primary_release_date.lte=${lte}&` : ''}${gte ? `first_air_date.gte=${gteAD}&` : ''}${lte ? `first_air_date.gte.lte=${lteAD}&` : ''}language=en-US&${searchQuery ? `query=${searchQuery}&` : ''}${page ? `page=${page}` : ''}&include_adult=true`;
+  // const url = `https://content.guardianapis.com/${category}?search?from-date=${currentDate}&page-size=10&show-fields=body%2Cthumbnail%2CshouldHideAdverts%2Cheadline%2C&show-elements=image&show-blocks=all&show-tags=contributor&api-key=${API_SECRET}`;
+
+  try {
+    const { data } = await axios.get(url)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+
+  }catch(error){
+    const {status, statusText, headers, data} = error.response;
+
+    return{
+      statusCode: status,
+      body: JSON.stringify({status, statusText, headers, data})
+    }
+    
+  }
+}
+
+module.exports = { handler }
